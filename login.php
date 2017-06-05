875eb7e3dafbc1d2e5db26cf39a6644fc368c030
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-06-05 14:11:24 +0900

    login画面html雛形作成

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>login | Database System 1</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Database System 2">
+    <meta name="author" content="Mori Atsushi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <header>
+      <h1>Online Shop</h1>
+      <h2>Database System I</h2>
+    </header>
+    <section>
+      <form action="./auth.php" method="post">
+        <input type="text" placeholder="ユーザID">
+        <input type="password" placeholder="パスワード">
+        <input type="submit" value="Login">
+      </form>
+      <div>or</div>
+      <a href="./singup.php">Sing Up</a>
+    </section>
+    <footer>
+      University of Tsukuba
+    </footer>
+  </body>
+</html>
