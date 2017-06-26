8f201bd09af7a81cf0f22cff96385a821bf15696
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-06-26 14:08:22 +0900

    index雛形作成

--- index.php
+++ index.php
@@ -3,6 +3,29 @@
   if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_type'])) {
     header('Location: ./login.php');
   }
-  echo $_SESSION['user_id'] . '<br>';
-  echo $_SESSION['user_type'] . '<br>';
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>login | Database System 1</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Database System 2">
+    <meta name="author" content="Mori Atsushi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <header>
+      <h1>Online Shop</h1>
+      <h2>Database System I</h2>
+    </header>
+
+    <section>
+
+    </section>
+
+    <footer>
+      University of Tsukuba
+    </footer>
+  </body>
+</html>
