a1da594dbbf731bbd5e0347f98277378bd0187ba
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-11 04:31:57 +0900

    利用者、販売業者の追加機能を実装

---
+++ api/regist-customer.php
@@ -0,0 +1,42 @@
+<?php
+  session_start();
+  if(isset($_POST['mail']) && $_POST['mail'] !== ''
+    && isset($_POST['password']) && $_POST['password'] !== ''
+    && isset($_POST['password2']) && $_POST['password2'] !== ''
+  ) {
+    if($_POST['password'] !== $_POST['password2']) {
+      $_SESSION['add_customer_error'] = 2;
+      header('Location: ../add-customer.php');
+      exit();
+    }
+
+    require_once('../config.php');
+    $link = mysqli_connect($dbserver, $user, $password, $dbname)
+      or die('MySQL への接続に失敗しました');
+    mysqli_set_charset($link, "utf8")
+      or die('文字コードの設定に失敗しました');
+
+    $userTypes = array('customer', 'seller', 'manager');
+    foreach($userTypes as $userType) {
+      $query = 'select * from ' . $userType . ' where mail = "' . $_POST['mail'] . '"';
+      $result = mysqli_query($link, $query)
+        or die('問い合わせの実行に失敗しました');
+      if(mysqli_num_rows($result) === 1) {
+        mysqli_close($link);
+        $_SESSION['add_customer_error'] = 1;
+        header('Location: ../add-customer.php');
+        exit();
+      }
+    }
+    $query = 'insert into customer(mail, pass) values("' . $_POST['mail'] . '", "' . $_POST['password'] . '")';
+    $result = mysqli_query($link, $query)
+      or die('問い合わせの実行に失敗しました');
+    $_SESSION['add_customer_error'] = 0;
+    header('Location: ../add-customer.php');
+    exit();
+  } else {
+    $_SESSION['add_customer_error'] = 3;
+    header('Location: ../add-customer.php');
+    exit();
+  }
+?>
