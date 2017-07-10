aef5e2ec7e450a12abe9fe1dfb72f258d29d049d
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-10 23:35:08 +0900

    商品詳細ページ、商品内容出力

---
+++ product-detail.php
@@ -0,0 +1,70 @@
+<?php
+  if(!isset($_GET['product_id'])) {
+    header('Location: ./index.php');
+    exit();
+  }
+
+  session_start();
+  if(!isset($_SESSION['user_id']) || !isset($_SESSION['user_type'])) {
+    header('Location: ./login.php');
+    exit();
+  }
+
+  require_once('config.php');
+  $link = mysqli_connect($dbserver, $user, $password, $dbname)
+    or die('MySQL への接続に失敗しました');
+  mysqli_set_charset($link, "utf8")
+    or die('文字コードの設定に失敗しました');
+
+  $query = '';
+  $query .= 'select * from product, seller ';
+  $query .= 'where product.user_id = seller.user_id ';
+  $query .= 'and product.product_id = "' . $_GET['product_id'] . '" ';
+  $result = mysqli_query($link, $query)
+    or die('問い合わせの実行に失敗しました');
+  if(mysqli_num_rows($result) === 0) {
+    header('Location: ./index.php');
+    exit();
+  }
+
+  $product = null;
+  while($row = mysqli_fetch_assoc($result)) {
+    $product = $row;
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Database System 1 | <?php echo $product['name']; ?></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Database System 2">
+    <meta name="author" content="Mori Atsushi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <header>
+      <h1><?php echo $product['name']; ?></h1>
+      <nav>
+        <ul>
+          <li>ログアウト</li>
+        </ul>
+      </nav>
+
+    <section>
+      <?php
+        echo '<img src="' . $product['image_url'] . '">';
+        echo '<h2>' . $product['name'] . '</h2>';
+        echo '<h3>' . $product['shop_name'] . '</h3>';
+        echo '<a href="./purchase?product_id=' . $product['product_id'] . '">購入</a>';
+        echo '<span>' . $product['price'] . '円</span>';
+        echo '<span>（在庫：' . $product['stock'] . '）</span>';
+        echo '<p>' . $product['comment'] . '</p>';
+      ?>
+    </section>
+
+    <footer>
+      University of Tsukuba
+    </footer>
+  </body>
+</html>
