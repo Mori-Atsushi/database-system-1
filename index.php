1821bb470f418f0e562ba85b02cfb01c48632d94
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-07-10 11:12:58 +0900

    indexの雛形作成
    html雛形作成
    sql文の追加

--- index.php
+++ index.php
@@ -24,7 +24,10 @@
   </head>
   <body>
     <header>
-      <input type="text" name="mail" placeholder="検索">
+      <form action="./search.php" method="post">
+        <input type="text" name="mail" placeholder="検索">
+        <input type="submit" value="検索">
+      </form>
       <nav>
         <ul>
           <li>ログアウト</li>
@@ -36,7 +39,13 @@
       <h2>人気商品</h2>
       <ul>
         <?php
-          $query = 'select * from product limit ' . PRODUCT_MAX_NUM;
+          $query = '';
+          $query .= 'select *, count(purchase.purchase_id) from product, purchase ';
+          $query .= 'where product.product_id = purchase.product_id ';
+          $query .= 'group by product.product_id ';
+          $query .= 'order by sum(purchase.value) desc ';
+          $query .= 'limit ' . PRODUCT_MAX_NUM;
+
           $result = mysqli_query($link, $query)
             or die('問い合わせの実行に失敗しました');
           while($row = mysqli_fetch_assoc($result)) {
@@ -64,11 +73,20 @@
       <h2>購入履歴</h2>
       <ul>
         <?php
-          $query = 'select * from product limit ' . PRODUCT_MAX_NUM;
+          $query = '';
+          $query .= 'select * from product, purchase ';
+          $query .= 'where product.product_id = purchase.product_id and purchase.user_id = ' . $_SESSION['user_id'] . ' ';
+          $query .= 'order by purchase.purchase_date desc ';
+          $query .= 'limit ' . PRODUCT_MAX_NUM;
+
           $result = mysqli_query($link, $query)
             or die('問い合わせの実行に失敗しました');
-          while($row = mysqli_fetch_assoc($result)) {
-            echo '<li><a><div>' . $row['name'] . '</div></a></li>';
+          if(mysqli_num_rows($result) === 0) {
+            echo '<p>購入履歴はありません</p>';
+          } else {
+            while($row = mysqli_fetch_assoc($result)) {
+              echo '<li><a><div>' . $row['name'] . '</div></a></li>';
+            }
           }
         ?>
       </ul>
